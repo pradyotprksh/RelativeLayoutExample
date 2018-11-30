8650cac0bbebddaf9cc7996d30c9938f65414a43
abc <abc@glai.com>
2018-11-30 20:08:23 +0530

    Async Task Done with ProgressBar

--- app/src/main/java/com/example/a3embed/relativelayoutexample/CoordinatorLayout/CoordinatorLayout.java
+++ app/src/main/java/com/example/a3embed/relativelayoutexample/CoordinatorLayout/CoordinatorLayout.java
@@ -1,17 +1,22 @@
 package com.example.a3embed.relativelayoutexample.CoordinatorLayout;
 
-import android.graphics.Color;
+import android.os.AsyncTask;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.a3embed.relativelayoutexample.R;
 
+import java.util.Arrays;
+
 public class CoordinatorLayout extends AppCompatActivity {
+
+    private ProgressBar determinateBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +25,9 @@ public class CoordinatorLayout extends AppCompatActivity {
 
         // Variables and initialization
         Button showSnackBar = findViewById(R.id.showSnackBar);
+        FloatingActionButton fab = findViewById(R.id.fab);
         final android.support.design.widget.CoordinatorLayout mainLayout = findViewById(R.id.mainLayout);
+        determinateBar = findViewById(R.id.determinateBar);
 
         // OnClickListener
         showSnackBar.setOnClickListener(new View.OnClickListener() {
@@ -31,11 +38,47 @@ public class CoordinatorLayout extends AppCompatActivity {
                         .setAction("CLOSE", new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
-                                finish();
+                                DoSomeTask task = new DoSomeTask();
+                                task.execute("Hello");
                             }
                         }).show();
             }
         });
 
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
+
+    class DoSomeTask extends AsyncTask<String, Integer, String> {
+
+        @Override
+        protected void onPreExecute() {
+
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            for (int i = 0; i < 100; i++) {
+                publishProgress (i);
+            }
+            return params[0];
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            String value = Arrays.toString(values);
+            determinateBar.setProgress(Integer.parseInt(value.replace("[", "").replace("]","")));
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
